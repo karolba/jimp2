b0e2473103485961397dfa62db6ca5dd1bfb414b
stardust99 <ada.gnacek@gmail.com>
2019-03-06 14:26:37 +0100

    yyy

--- JMP1/main.cpp
+++ JMP1/main.cpp
@@ -5,5 +5,11 @@ using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
+    string input, binput;
+    while(getline(cin, binput))
+    {
+         input += binput;
+    }
+    cout << endl << input << endl;
     return 0;
 }
