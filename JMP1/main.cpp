0f110f12b745e2527a1b0e06c85ea5621e09cc4b
Karol Baraniecki <karol@baraniecki.eu>
2019-03-06 14:21:09 +0100

    add expression

--- JMP1/main.cpp
+++ JMP1/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <regex>
 
 using namespace std;
+
+#define D       "[0-9]"
+#define NL      "((\n)|(\r)|(\r\n))"
+#define IDENT   "([a-zA-Z][a-zA-Z0-9]*)"
+#define EXPON   "([Ee][+-]?{D}+)"
+#define NUMBER  "(({D}+\.?{D}*{EXPON}?)|(\.{D}+{EXPON}?))"
 
 int main()
 {
