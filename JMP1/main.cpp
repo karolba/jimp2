7506eb4e8f3b993edb0ed2da9d3e52a653b6b6e1
Karol Baraniecki <karol@baraniecki.eu>
2019-03-06 16:05:08 +0100

    first: done

--- JMP1/main.cpp
+++ JMP1/main.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include <regex>
+#include <vector>
 
 using namespace std;
 
 #define D       "[0-9]"
-#define NL      "((?:\n)|(?:\r)|(?:\r\n))"
+#define NL      "((?:\\n)|(?:\\r)|(?:\\r\\n))"
 #define IDENT   "([a-zA-Z][a-zA-Z0-9]*)"
 #define EXPON   "(?:[Ee][+-]?" D "+)"
 #define NUMBER  "((?:" D "+\\.?" D "*" EXPON "?)|(?:\\." D "+" EXPON "?))"
 
-#define WHOLE "(?:" NUMBER "|" IDENT "|" NL "|(,)|(;)|([(])|([)])|(=)|(+)|(-)|(*)|(/)|( ))"
+#define WHOLE "(?:" NUMBER "|" IDENT "|" NL "|(,)|(;)|([(])|([)])|(=)|(\\+)|(-)|(\\*)|(/)|(\\^))"
 
 enum TYP {
+    TYP_NULL,
+
     TYP_NUMERIC,
     TYP_IDENTIFIER,
     TYP_ENDLINE,
@@ -28,6 +31,7 @@ enum TYP {
 };
 
 const char *name[] = {
+    "",
     "Numeric",
     "Identifier",
     "Endline",
@@ -43,8 +47,38 @@ const char *name[] = {
     "Exponential"
 };
 
+class Token {
+private:
+   TYP type;
+   string content;
+public:
+
+   Token(TYP typ, const string &s) {
+       this->type = typ;
+       content = s;
+   }
+
+   TYP getType() {
+       return type;
+   }
+
+   string getName() {
+       return name[getType()];
+   }
+
+   string getContent() {
+       return content;
+   }
+
+   void wypisz() {
+       cout << getName() << ": " << getContent() << '\n';
+   }
+};
+
 int main()
 {
+    vector<Token> tokens;
+
     cout << "Hello World!" << endl;
     string input, binput;
     while(getline(cin, binput))
@@ -56,12 +90,22 @@ int main()
     regex token(WHOLE);
     smatch sm;
 
-    if(regex_search(input.cbegin(), input.cend(), sm, token)) {
+    auto tokens_begin = sregex_iterator(input.cbegin(), input.cend(), token);
+    auto tokens_end = sregex_iterator();
+
+    for (sregex_iterator i = tokens_begin; i != tokens_end; ++i) {
+        smatch sm = *i;
         for(size_t i = 1; i < sm.size(); i++) {
-            if(sm.position(i) == 0) {
-                cout << name[i] << ": " << sm[i] << '\n';
+            if(! sm[i].str().empty()) {
+                Token token(static_cast<TYP>(i), sm.str());
+                tokens.push_back(token);
             }
         }
     }
+
+    for(Token &token : tokens) {
+        token.wypisz();
+    }
+
     return 0;
 }
