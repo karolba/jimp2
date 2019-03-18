e820dd50044b689ff917a08a32fad7f7b397ed23
stardust99 <ada.gnacek@gmail.com>
2019-03-18 12:07:52 +0100

    halohalko

--- ZAD3/main.cpp
+++ ZAD3/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
-
+#include <map>
 using namespace std;
 
 int main()
 {
+    int num = 0, liczba1=0;
+    map <int,int> m;
     cout << "Hello World!" << endl;
+    cin >> liczba1;
+    for (int i=0; i<liczba1; i++)
+    {
+    cin >> num;
+    if(m.find(num)==m.end())
+            {m[num]=0;}
+    m[num]++;
+    }
     return 0;
 }
